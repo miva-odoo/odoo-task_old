b64d7077a25394077995c8b6f876f771044e2b8b
miva-odoo <miva@odoo.com>
2022-03-01 08:22:54 +0530

    [mod] sale_stock_approval module

--- sale_stock_approval/models/sale_order.py
+++ sale_stock_approval/models/sale_order.py
@@ -18,9 +18,7 @@ class SaleOrder(models.Model):
     # method declaration
     #----------------------
    
-    # def _get_value(self):
-    #   ,compute='_get_value'
-    #     self.zero_stock_approval = True
+    
 
     # @api.constrains('zero_stock_approval')
     # def _zero_stock_approval(self):
@@ -31,16 +29,14 @@ class SaleOrder(models.Model):
 
     def action_confirm(self):
      for record in self:
-         if record.zero_stock_approval != True :
+         if not record.zero_stock_approval:
              raise UserError ("Approval are not True pls first true approual")
          else:
              return super(SaleOrder, self).action_confirm()
 
     @api.depends('zero_stock_approval')
-    def _compute_user_group(self) :
-        
-        if self.env.user.has_group('sales_team.group_sale_manager'):
-            
+    def _compute_user_group(self) :     
+        if self.env.user.has_group('sales_team.group_sale_manager'):  
             self.bool_approval = True
         else :
             self.bool_approval = False
