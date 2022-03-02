ab916a96ae2b21e02f95932002f83092a4af016d
miva-odoo <miva@odoo.com>
2022-03-02 10:56:25 +0530

    [Development] task_separation module

---
+++ task_separation/__manifest__.py
@@ -0,0 +1,26 @@
+{
+#imfromation
+    
+ 'name':'Task Sepration',
+ 'version': '15.0',
+ 'summary': 'Task Sepration',
+ 'description': 'Task Sepration',
+ 'category': 'custom',
+
+# Author
+
+ 'author': 'odoo Ps',
+ 'website': 'https://www.odoo.com/',
+ 'license': '',
+
+# Dependency
+ 'depends': ['project'],
+ 'data': [
+     'views/project_task_view.xml',
+     
+ ],
+
+ # Other
+ 'installable': True,
+ 'auto_install': False,
+}
---
+++ task_separation/models/project_task.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields,models
+
+class ProjectTask(models.Model):
+    _inherit='project.task'
+
+    #----------------------------
+    #  fields Declaration
+    #---------------------------
+
+    
+    
